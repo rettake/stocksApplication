9fc7b4d0c919db325ab810c8e34ebd79d9dd2a93
Vlad Okhotnikov <rettakecsgo@gmail.com>
2022-09-29 18:12:06 +0300

    React Router

--- stocks-app/src/App.jsx
+++ stocks-app/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+import StockDetailsPage from './pages/StockDetailsPage';
+import StockOverviewPage from "./pages/StockOverviewPage";
 
 function App() {
   return (
     <div className="App">
-      <h2 className="">Hello there</h2>
+      <BrowserRouter>
+        <Routes>
+            <Route
+                path='/'
+                element={<StockOverviewPage />}
+            />
+            <Route
+                path='/details:symbol'
+                element={<StockDetailsPage />}
+            />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- stocks-app/src/pages/StockOverviewPage.jsx
+++ stocks-app/src/pages/StockOverviewPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StockOverviewPage = () => {
     return (
         <div>
-            
+            StockOverviewPage
         </div>
     );
 };
